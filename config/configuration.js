8617611806ef8502ab2c9a49021b051ea0419a92
Dungji zacks <54458614+checkycheck@users.noreply.github.com>
2024-07-12 01:28:13 +0100

    Update configuration.js

--- config/configuration.js
+++ config/configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
-    mongoDbUrl: 'mongodb://localhost:27017/apro-tech1',
-    // mongoDbUrl:'mongodb://aprotech:1256@cluster0-shard-00-00-20i6o.mongodb.net:27017,cluster0-shard-00-01-20i6o.mongodb.net:27017,cluster0-shard-00-02-20i6o.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+    // mongoDbUrl: 'mongodb://localhost:27017/apro-tech1',
+    mongoDbUrl:'mongodb+srv://coza:6045@cluster0.ikkyv.mongodb.net/pay4servicedb?retryWrites=true&w=majority',
     PORT: process.env.PORT || 4194,
     globalVariables: (req, res, next) =>{
         res.locals.success_message = req.flash('success-message');
