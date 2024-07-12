dde151d863de971994268ba0ece1d50249065d0b
Dungji zacks <54458614+checkycheck@users.noreply.github.com>
2024-07-12 01:32:15 +0100

    Update configuration.js

--- config/configuration.js
+++ config/configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
     // mongoDbUrl: 'mongodb://localhost:27017/apro-tech1',
-    mongoDbUrl:'mongodb+srv://coza:6045@cluster0.ikkyv.mongodb.net/pay4servicedb?retryWrites=true&w=majority',
+    mongoDbUrl:'mongodb+srv://coza:6045@cluster0.ikkyv.mongodb.net/aprodb?retryWrites=true&w=majority',
     PORT: process.env.PORT || 4194,
     globalVariables: (req, res, next) =>{
         res.locals.success_message = req.flash('success-message');
